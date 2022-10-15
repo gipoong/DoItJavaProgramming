aaef1f3d1444bbf4eb137ea4ec571263da6bbc73
gipoong <gipoong@naver.com>
2022-10-15 18:09:17 +0900

    chapter 9

---
+++ src/chapter9/Car.java
@@ -0,0 +1,7 @@
+package chapter9;
+
+public abstract class Car {
+    public abstract void run();
+    public abstract void refuel();
+    public abstract void stop();
+}
---
+++ src/chapter9/Car2Test.java
@@ -0,0 +1,13 @@
+package chapter9;
+
+public class Car2Test {
+    public static void main(String[] args) {
+        System.out.println("=== 자율 주행하는 자동차 ===");
+        Car2 myCar = new AICar();
+        myCar.run();
+
+        System.out.println("=== 사람이 운전하는 자동차 ===");
+        Car2 hisCar = new ManualCar();
+        hisCar.run();
+    }
+}
